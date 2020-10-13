15f964f166a979d58389da49b30be6943bc6904e
letronsrs <ricky.wang@letronlab.com>
2020-10-13 18:03:59 +0800

    add api for lg

--- backend/server/apps/api/views.py
+++ backend/server/apps/api/views.py
@@ -8,48 +8,77 @@ from worker.OpenApiDemo import OpenApiDemo
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from celery import Celery
+from worker.mlog import Log
 
 import os
 import re
 import json
 import logging
 import urllib
-
 from time import gmtime, strftime
-
 
 WORKERS = Celery('simple_worker')
 WORKERS.config_from_object('celeryconfig')
 
 
-# Create your views here.
-class Log(object):
-    def __init__(self,log_file_name,log_name):
-        self.logfilename = "%s%s.log"%(log_file_name,strftime("%Y%m%d%H%M", gmtime()))
-        self.logname = log_name
-        self.logger = self.__set_log()
-
-
-    def __set_log(self):
-        logpath = os.path.join(os.getcwd(), 'log')
-        if not os.path.exists(logpath):
-            os.makedirs(logpath)
-        filepath = os.path.join(logpath, self.logfilename)
-        logger = logging.getLogger(self.logname)
-        logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fh = logging.FileHandler(filepath)
-        fh.setLevel(logging.INFO)
-        fh.setFormatter(formatter)
-        logger.addHandler(fh)
-        console = logging.StreamHandler()
-        console.setLevel(logging.DEBUG)
-        console.setFormatter(formatter)
-        logger.addHandler(console)
-        return logger
-
-
 log = Log("api","run_testing")
+def lg_get_nodes(request):
+    try:
+       if request.method == 'GET':
+           username = request.GET['username']
+           token =  request.GET['token']
+           api =  lg_operation_api(operapihost)
+           userpofile =  api.get_userprofile(token,username)
+
+           result = list()
+           monitorlist = api.get_instances(token,customer.instance)
+           for monitor in monitorlist:
+               result.append({'nid':monitor.id,'host_name': monitor.host_name,'region':monitor.region_name,'isp':monitor.isp_name,'host_ch_name':monitor.ch_name})
+           return HttpResponse(json.dumps(result), content_type="application/json")
+       else:
+            message = 'Please use get to access'
+            return HttpResponse(message,status=status.HTTP_400_BAD_REQUEST)
+
+    except Exception as e:
+        message =  "Exception : "+ str(e)
+        return HttpResponse(message,status=status.HTTP_400_BAD_REQUEST)
+
+def lg_run_command(request):
+    try:
+       if request.method == 'POST':
+            log = Log("api","api")
+            username = request.GET['username']
+            token =  request.POST['token']
+            if token and username :
+                nid = int(request.POST['nid'])
+                lg_command = request.POST['command']
+                lg_host = request.POST['host']
+                lg_origin = request.POST['lg_origin']
+                curl_domain = request.POST['curl_domain']
+                curl_port= request.POST['curl_port']
+                curl_ip= request.POST['curl_ip']
+                tcp_port = request.POST['tcp_port']
+                nslookup_type = request.POST['nslookup_type']
+                nslookup_dns = request.POST['nslookup_dns']
+
+                api =  lg_operation_api(operapihost)
+                userpofile =  api.get_userprofile(token,username)
+                node =  api.get_instance(token,lg_id)
+
+                lgnodeapi = lg_nodes_api(node.region_name,node.ch_name,node.host_ip)
+                response_data = lgnodeapi.get_nodes_api(lg_command,lg_host,lg_origin,curl_domain,curl_port,curl_ip,tcp_port,nslookup_type,nslookup_dns)
+            
+            return HttpResponse(response_data, content_type="application/json")
+
+       else:
+            message = 'Please use get to access'
+            return HttpResponse(message,status=status.HTTP_400_BAD_REQUEST)
+
+    except Exception as e:
+        message =  "Exception : "+ str(e)
+        return HttpResponse(message,status=status.HTTP_400_BAD_REQUEST)
+
+'''
 @csrf_exempt
 def runtool(request):
     try:
@@ -76,3 +105,4 @@ def runtool(request):
         message =  "Exception : "+ str(e)
         return HttpResponse(message,status=status.HTTP_400_BAD_REQUEST)
 
+'''
